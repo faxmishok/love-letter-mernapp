eaae28ce5cbfdfc9f0c4c514e400658109086c55
Nauboys <muhammad.samadzade@gmail.com>
2020-04-25 20:07:17 +0400

    few changes

--- react-login/src/components/login.component.js
+++ react-login/src/components/login.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../App.css';
+import './login.css';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
--- react-login/src/components/signup.component.js
+++ react-login/src/components/signup.component.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../App.css';
+import './sigup.css';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default class SignUp extends Component {
     render() {
         return (
-            <div className="App">
+    <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
           <Link className="navbar-brand" to={"/sign-in"}>Sign-in</Link>
